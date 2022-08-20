79f46bc9e0efe2c5fabd3cb68940e3be95e8a832
nikhil upadhyay <nikhil.upadhyay_cs19@gla.ac.in>
2022-08-20 11:08:19 +0530

    Added promises to the API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 function App() {
+
+
+  const [coins,setCoins] = useState([]);
+
+  // Adding Function for the API
+  useEffect(() =>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      setCoins(res.data);
+    })
+    .catch(err => console.log(err))
+  },[]);
+
   return (
     <div className="App">
       <h1>API</h1>
