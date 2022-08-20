1ba26a51ac4c183fc13154ba272384607029a545
nikhil upadhyay <nikhil.upadhyay_cs19@gla.ac.in>
2022-08-20 13:53:46 +0530

    edited the coin component

--- src/Coin.js
+++ src/Coin.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const Coin = () => {
+const Coin = ({names,image,symbol,price,volume}) => {
   return (
-    <div>Coin</div>
+    <div className='coin-container'> Coin
+        <div className='coin-row'>
+            <div className ="coin">
+                <img src={image} alt ="crypto"></img>
+                <h1>{names}</h1>
+                <p className='coin-symbol'>{symbol}</p>
+            </div>
+            <div className="coin-data">
+                <p className="coin-price">${price}</p>
+                <p className="coin-volume">${volume.toLocaleString()}</p>
+            </div>
+        </div>
+
+
+    </div>
   )
 }
 
