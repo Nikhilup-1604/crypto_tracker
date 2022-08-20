2ee04eaae010e08254c995c9f6ef24ed1c26f297
nikhil upadhyay <nikhil.upadhyay_cs19@gla.ac.in>
2022-08-20 11:23:48 +0530

    start adding JSX

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ function App() {
 
   // Adding Function for the API
   useEffect(() =>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    axios
+    .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
       setCoins(res.data);
+      console.log(res.data);
     })
     .catch(err => console.log(err))
   },[]);
